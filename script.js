b1155a83c3e1ea48201bc0b7643ceb45f7030344
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-05-21 23:52:59 +0530

    2 commit

---
+++ script.js
@@ -0,0 +1,100 @@
+
+///////practice\\\\\\\\\\\\\\\\\\\\\\
+
+// function my(see){
+// console.log(see)
+// }
+
+// my("aditi");
+
+// class hello{
+//           message() {
+//             console.log("hwllo everyone")
+//           }
+// sorry(){
+// console.log("sorry everyoenggugug")
+// }
+// }
+
+// let a = new hello();
+// a.message()
+
+// a.sorry()
+
+
+// let name = ["adit", "adj", "aes", "asxz"]
+// for(let i=0; i<name.length; i++){
+//   console.log(name[i])
+// }
+
+// let cities=['bhopal',"delhi","mumbai","kolkata"];
+
+// for(let city of cities){
+// console.log(city.toUpperCase())
+// }
+
+// let sum= [85, 97, 44, 37, 76, 60];
+// let marks=0
+
+// for(let no of sum){
+//   marks+=no
+
+
+// }
+// let avg= marks/sum.length
+
+// console.log(`avrg marks of student is  = ${avg}`)
+
+
+// let  items = [250, 645 , 300 , 900 , 50]
+
+// let idx = 0;
+
+// for(let val of items ){
+// console.log(`the value of ${idx} = ${items}`)
+
+// }
+
+
+// class hey{
+//           message(){
+//             console.log("Hello eveeryone ,,its aditi ");
+//           }
+// call(){
+//     console.log("i am expert in coding >.....")
+// }
+
+// hello(){
+//     console.log("hii \"its expert\" ")
+// }
+
+// }
+
+// let a = new hey ();
+// a.message()
+// let s = new hey
+// s.call()
+
+// let d = new hey
+// d.hello()
+////////////////////(//////////      19 05 2024     /////////////////////////////////
+
+// class worker{
+// constructor(){
+//   console.log(" ")
+// }
+// }
+let z = {
+  name: "aditi",
+  language: "javascript"
+}
+console.log(z)
+
+let a = {
+    run :  ()=>{
+        alert("helllooooooooooo")
+    }
+}
+
+z._____proto________= a
+con
