8b966e46436593e420006b72399142ee649a9a9b
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-06-25 17:43:50 +0530

    20 commit

--- script.js
+++ script.js
@@ -1,200 +1,3 @@
-
-//////////////////////////////////////////// sepicific no find krne ke liye /// true or false main ans aata hai iska 
-// let na = [1,2,9,3,5, 'aditi'  , 4 , "raju" , "kana"]
-// console.log(na.includes(8))
-//////////////////////
-// let a = {
-//      name : 'aditi'
-// }
-// console.log(a)
-
-///////////////////////////// remove sepecific value  using of pop push slice splice
-// let a = [1,2,3,4,5,6,7];  /// push  add the element in the endd /// pop remove the value from the end 
-//  a.splice(1,3, )    /// slice()  two parts main he cut hote h (1,3) jo third index h woh include nhi hoge aur ye nhi array  main aajayega
-// console.log(a)          /// splice   main (first starting point , kya delet krna h  ,  "kya daln H ")
-//// shift( ) remove th first elemeents in js 
-
-
-//primitive data type  -- boolean null nmber  stirng 
-// let a = "aditi";
-// let b  = a;
-// b = "abhi"
-// console.log(a)
-//  NON --- primitive data type  ---- array ,, object 
-// let obj =  {
-//      name : 'aditi',
-//      class : "pass ",
-//      pass : 255
-// }
-
-// let obj2 = obj;
-// obj2.name = "shilpa"
-// console.log(obj2)
-// console.log(obj)
-
-//// to find frequency  elements
-//  let q = [1,2,3,1,2,3,1,2,3,1,1,1,]
-//  let w = {}
-
-//  q.forEach(elem => {
-//      if (!w[elem]) {
-//           w[elem] = 1
-//      } else {
-//           w[elem] = w[elem] + 1
-//      }
-//  });
-//  console.log(w)
-
-// let q = [1,2,3,1,2,3,1,]
-// let w = {}
-// for (const i of q) {
-//      if (w[i]) {
-//          w[i] +=1
-//      } else {
-//           w[i] =  1
-//      }
-// }
-// console.log(w)
-
-//////////////   first and last elements of an array 
-// let a = ["adiiti" , "raj" , 1, 3,4, "hum "]
-// let r = a.pop()
-// a.shift()
-// console.log(r)
-// console.log(a.shift())
-/////////////////////////
-// let a = ["adiiti" , "raj" , 1, 3,4, "hum "]
-// let w = {}
-// for(let i of w) {
-//   a.concat(w)
-// }
-// console.log(a)
-////////////////   to find all falsy values    only one  method is use in this 
-// let a = ["" , "raj" , 1, 3,4,  undefined, null, 3 , 6 ,"", 9]
-// let w = a.filter(Boolean)
-// console.log(w)
-//////////////////////      To check positive no 
-// let no = [ 1,2,3,4,-2,-3,-5,-6,-7,-5,5,3,7,3,-4]
-// // function aee(no){
-// //      let no1 = no.filter(x=>x>-1)
-// // }
-// // console.log(no)
-// // no.sort(function(a,b) {
-// //      return b - a;
-// //   });
-//   let x = no.length
-//   for (x; x--;) {
-//      if ( no[x] < 0 ){
-//         no.pop();
-//      }
-//   }
-//   console.log(no)
-
-//  10- question 
-// let a = prompt("please enter element")
-// function aee(a){
-// let reverse = a.split("").reverse().join("")
-// return reverse == a
-// }
-
-
-// console.log(aee(a))
-
-//////////////////////////////// 9 
-/// to reverse a string 
-// let a = prompt("What is your name ? ")
-// // let a  = {}
-// let a2 = a.split("").reverse().join("");
-// console.log(a2)
-
-// let number = prompt ("give only even no ")
-
-// function arr(number){
-//     if (number%2===0 ){
-//       console.log("it is an even no buddy .. u are too good  ")
-//     }else {
-//         console.log("it is not even no ... what are u doing ")
-//     }
-// }
-// console.log(arr(prompt("Give Even no budy !!"))) //////////  ye un degine de rha h 
-
-////////////////////////////////////////////
-// let num = prompt("give positive no ??")
-
-// if (num==0) {
-//   console.log ( `factorial of ${num} is 1`)
-// } else if(num<0){
-//   console.log(`neagative no is not alowed ${num}`)
-// }
-// else{
-//   let blanck = 1;
-// for (let index = 1 ; index <=num; index++) {
-//  blanck = blanck * index
-// //  console.log(`factorial of ${num}  is ${blanck}`) 
-// }
-// console.log(`factorial of ${num}  is ${blanck}`) 
-// }
-
-
-/////////////////////////////////////////////////////////
-// let num = prompt("give any a prime  no ??")
-
-// if(num == 1){
-//   console.log(`${num} is not prime no `)
-// }
-// if(num == 2){
-// console.log("it is prime no ")
-// }
-// else if(num <= 0 ){
-// console.log(`${num} no is invalid `)
-// }
-// else{
-//   for(let i = 2 ; i <= num; i++)
-//     if (num%i == 0) {
-//       var res = `${num} is not prime no `
-//       break
-//     } else {
-//       var res = `${num} is a prime no `
-//      }
-// console.log(res)
-// }
-
-////////////////////////////////////////////////////////////////////////////
-//  REVISE THE PREVIOUS CODE 
-// let a = prompt("give name ")
-// function arr(a){
-//   let w = a.split("").reverse().join("")
-//    return w
-// }
-// console.log(arr(a))
-
-///////////////////////////////////
-// let a = prompt("give ")
-// function arr(a){
-//   let k = a.split("").reverse().join("")
-//   return k ===a
-// }
-// console.log(arr(a))
-
-////////////////////////////////////////////////////
-// let a = prompt("give value")
-// if (a%2==0) {
-//   console.log("it is even ")
-// } else {
-//   console.log("it is not")
-// }
-/////////////////////////////////////////////////
-// let a = [1,2,3,4,5,6,7,8,9,10]
-// let b =[];
-// let c = []
-// for (let i = 0; i < a.length; i++) {
-//   const element = a[i];
-//   if (element%2==0) {
-//     b.push(element)
-//   } else {
-//     c.push(element)
-//   }
-// }console.log(b )
 // console.log(c)
 /////////////////////////////////////////////////////////// factorial of 
 // let num = prompt("give any no")
@@ -586,30 +389,65 @@
 // } console.log(position)
 
 ////////////////////////////   recursive binary code 
-function recursive(arr, find) {
-    return search(arr, find, 0, arr.length - 1)
+// function recursive(arr, find) {
+//     return search(arr, find, 0, arr.length - 1)
 
-}
+// }
 
-function search(arr, find, start, end) {
-    if (start > end) {
-        return -1
-    }
-
-
-let mid = Math.floor((start + end  )  /  2 )
-if (arr[mid] === find) {
-    return mid
-}
+// function search(arr, find, start, end) {
+//     if (start > end) {
+//         return -1
+//     }
 
 
-if (arr[mid] >  find) {
-      return     search(arr, find, start,  mid-1   )                                        //  return end = mid - 1
-} else {
-     return   search(arr, find,  mid + 1,   end   )                                       //return   start = mid + 1
-}
-}
-console.log(recursive([1, 2, 3, 4, 5, 6, 7, 8, 9], 6) )    //5
-console.log(recursive([1, 2, 3, 4, 5, 6, 7, 8, 9], 5))          // 4
-console.log(recursive([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))             // 2
+// let mid = Math.floor((start + end  )  /  2 )
+// if (arr[mid] === find) {
+//     return mid
+// }
 
+
+// if (arr[mid] >  find) {
+//       return     search(arr, find, start,  mid-1   )                                        //  return end = mid - 1
+// } else {
+//      return   search(arr, find,  mid + 1,   end   )                                       //return   start = mid + 1
+// }
+// }
+// console.log(recursive([1, 2, 3, 4, 5, 6, 7, 8, 9], 6) )    //5
+// console.log(recursive([1, 2, 3, 4, 5, 6, 7, 8, 9], 5))          // 4
+// console.log(recursive([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))             // 2
+
+////////////////////////////////////  re practice of code //////
+//  function binaryreecursive(arr, find) {
+//     return search(arr, find, 0, arr.length - 1)
+// }
+// function search(arr, find, start, end) {
+
+//     if (start > end) {
+//         return -1
+//     }
+
+
+
+// let mid = Math.floor((start + end) / 2)
+// if (arr[mid] === find) {
+//     return mid;
+// }
+
+// if (arr[mid] > find) {
+//     return search(arr, find, start, mid -1)
+
+// } else {
+
+//     return search(arr, find, mid +1, end)
+// }
+// }
+
+// console.log(binaryreecursive([1, 2, 3, 4, 5, 6, 7, 8], 8))  /// 7
+// console.log(binaryreecursive([1, 2, 3, 4, 5, 6, 7, 8], 1))   //  0
+// console.log(binaryreecursive([1, 2, 3, 4, 5, 6, 7, 8], 22))   // -1
+// console.log(binaryreecursive([1, 2, 3, 4, 5, 6, 7, 8], 4)) 
+
+////////////   4 types of sorting algorithems
+// let a =[1, 2, 3, 4,-5, 6, -7, 8]
+// let a1 = a.sort((x,y)  => x-y)
+// console.log(a1)
